c07db68c4da6b0a314252912534f54f9e0da00e6
Mitchell <mitchell.alton@live.com>
2020-04-13 22:55:29 -0500

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -6,12 +6,49 @@ import Table from './containers/Table';
 const API = "http://localhost:3000/sushis"
 
 class App extends Component {
+  
+  state = {
+    sushiList: [],
+    sliceStart: 0,
+    sliceEnd: 4,
+  }
+
+  componentDidMount(){
+    fetch(API)
+      .then(resp => resp.json())
+      .then(sushis => {
+        this.setState({
+          sushiList: sushis,
+          displaySushiList: sushis,
+          eatenSushi: [],
+          money: 100
+        })
+        // console.log(sushis)
+      })
+  }
+
+
+moreSushi = () =>{
+  this.setState({
+    sliceStart: this.state.sliceStart + 4,
+    sliceEnd: this.state.sliceEnd + 4
+  })
+}
+
+deleteSushi = (sushi) =>{
+  this.setState({
+    displaySushiList: this.state.displaySushiList.filter(s => s.id !== sushi.id),
+    eatenSushi: [...this.state.eatenSushi, 1],
+    money: this.state.money - sushi.price
+  })
+}
 
   render() {
+    console.log(this.state.sushiList)
     return (
       <div className="app">
-        <SushiContainer />
-        <Table />
+        <SushiContainer displaySushiList={this.state.displaySushiList} sliceStart={this.state.sliceStart} sliceEnd={this.state.sliceEnd} moreSushi={this.moreSushi} deleteSushi={this.deleteSushi} money={this.state.money}/>
+        <Table eatenSushi={this.state.eatenSushi} money={this.state.money}/>
       </div>
     );
   }
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,16 +1,19 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
 
 const SushiContainer = (props) => {
+  // console.log(props)
+
+
   return (
     <Fragment>
       <div className="belt">
         {
-          /* 
-             Render Sushi components here!
-          */
+        //  props.sushiList ? props.sushiList.map(sushi => <Sushi sushi={sushi}/>) : "Loading..."
+         props.displaySushiList ? props.displaySushiList.slice(props.sliceStart, props.sliceEnd).map(sushi => <Sushi sushi={sushi} deleteSushi={props.deleteSushi} money={props.money}/>) : "Loading..."
         }
-        <MoreButton />
+        <MoreButton moreSushi={props.moreSushi}/>
       </div>
     </Fragment>
   )
